6f2f494b8b84595cd41b58c9a50b762e66e47356
Jordan Garcia <jordan.empresa@outlook.com>
2023-08-30 23:11:32 -0600

    V2

---
+++ js/backEnd.js
@@ -0,0 +1,4 @@
+document.querySelector(".btn-mamalon").addEventListener("click", function () {
+  document.querySelector(".p-mamalon").classList.toggle("hide");
+  document.querySelector(".p-mamalon").classList.toggle("transform-animation");
+});
